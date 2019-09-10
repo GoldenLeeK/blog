053168930c8c8a37dda52a932aa3a02fb0f4fa67
GoldenLee <614580148@qq.com>
2019-09-10 16:06:12 +0800

    替换smarty模板渲染数据

--- index/index.php
+++ index/index.php
@@ -6,8 +6,9 @@ namespace index;
 
 use  libs\Db;
 
-
 session_start();
+//加载Smarty初始化类
+require dirname(__DIR__) .'/init.inc.php';
 //自动加载类
 require_once dirname(__DIR__) . '/autoload.php';
 $db = Db::getInstance();
@@ -28,57 +29,3 @@ if ($cid != 0) {
 //分类标签页按钮样式
 $label = array('btn-primary', 'btn-success', 'btn-info', 'btn-warning', 'btn-default', 'btn-danger');
 ?>
-<?php include_once './common/title.php' ?>
-    <title>个人博客</title>
-
-</head>
-<body>
-<?php include_once "./common/header.php" ?>
-
-<!--content中心内容-->
-<div class="container">
-    <div>
-        <div class="left_content_div col-md-8">
-            <?php
-            if (!empty($data['result'])) {
-                foreach ($data['result'] as $article) { ?>
-                    <div class="article">
-                        <h3>
-                            <a href="./details.php?aid=<?php echo $article['id'] ?>"><?php echo $article['article_title'] ?></a>
-                        </h3>
-                        <p>
-                            <?php echo $article['article_desc'] ?>
-                        </p>
-                        <div class="info_box">
-                            <span>作者：<?php echo $article['article_author'] ?></span>&nbsp;
-                            <span>时间：<?php echo date('Y-m-d H:i:s', $article['article_upload_time']) ?></span>
-                            <span>浏览量：<?php echo $article['article_visit_count'] ?>次</span>
-                        </div>
-                    </div>
-
-                <?php }
-            } else {
-                ?>
-                <div class="div_error">
-                    <div class="alert alert-info text-center" role="alert">抱歉，暂时没有文章......</div>
-                    <a href="./index.php" style="float: right">返回首页</a>
-                </div>
-            <?php } ?>
-        </div>
-
-    </div>
-    <!--    right-->
-    <?php include_once "./common/right.php" ?>
-</div>
-<!--分页-->
-<div>
-    <?php echo $data['pages']; ?>
-</div>
-<!--/content-->
-
-<!--footer-->
-<?php include_once "./common/footer.php" ?>
-<!--/footer-->
-</body>
-
-</html>
--- init.inc.php
+++ init.inc.php
@@ -5,3 +5,14 @@
  * Date: 2019/9/10
  * Time: 15:36
  */
+define('ROOT', str_replace('\\', '/', dirname(__FILE__)) . '/');
+require ROOT . 'libs/Smarty.class.php';
+$smarty = new Smarty();
+$smarty->setTemplateDir(ROOT . 'index/view')
+        ->addTemplateDir(ROOT . 'admin/view')
+        ->setCompileDir(ROOT . 'view_c')
+        ->setPluginsDir(ROOT . 'libs/puugins')
+        ->setCacheDir('view_cache')
+        ->setConfigDir(ROOT . 'smarty_configs');
+$smarty->caching = false;
+$smarty->cache_lifetime = 60 * 60 * 24;
