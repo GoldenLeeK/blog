462e39495db75809c005de9a721bcac2eea9b064
GoldenLee <614580148@qq.com>
2019-09-11 15:40:43 +0800

    设置缓存true

--- init.inc.php
+++ init.inc.php
@@ -10,13 +10,14 @@ define('ROOT', str_replace('\\', '/', dirname(__FILE__)) . '/');
 require ROOT . 'libs/Smarty.class.php';
 
 $smarty = new Smarty();
+$smarty->caching = true;
 $smarty->setTemplateDir(ROOT . 'index/view')
         ->addTemplateDir(ROOT . 'admin/view')
         ->setCompileDir(ROOT . 'view_c')
         ->setPluginsDir(ROOT . 'libs/plugins')
-        ->setCacheDir('view_cache')
+        ->setCacheDir(ROOT.'view_cache')
         ->setConfigDir(ROOT . 'smarty_configs');
-$smarty->caching = false;
+
 $smarty->cache_lifetime = 60 * 60 * 24;
 
 
