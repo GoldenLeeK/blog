7cef13238e2d5c5acc8aaf4df3d0c381d96e9416
GoldenLee <614580148@qq.com>
2019-09-14 12:12:30 +0800

    利用smarty渲染数据进行模板分类

--- admin/index.php
+++ admin/index.php
@@ -6,31 +6,16 @@
  * Time: 15:19
  */
 
+use  libs\Db;
 
 //自动加载类(smarty)
-require dirname(__DIR__) .'/init.inc.php';
+require dirname(__DIR__) . '/init.inc.php';
 
 session_start();
-    if (!isset($_SESSION['user'])){
-        echo "<script>alert('请登陆后访问！');location.href='./login.php'</script>";
-    }
+if (!isset($_SESSION['user'])) {
+    echo "<script>alert('请登陆后访问！');location.href='./login.php'</script>";
+}
+$db = Db::getInstance();
+$smarty->assign('ROOT', 'http://192.168.33.10/blog');
+$smarty->display('admin_index.html');
 ?>
-<?php include_once './common/title.php'; ?>
-    <title>个人博客后台中心</title>
-</head>
-<body>
-<div class="row">
-    <?php require_once './common/header.php' ?>
-    <!--侧边栏-->
-    <?php require_once  './common/left.php'?>
-    <!--主页面-->
-    <div id="main">
-
-
-    </div>
-
-    <?php require_once  './common/footer.php'?>
-</div>
-
-</body>
-</html>
